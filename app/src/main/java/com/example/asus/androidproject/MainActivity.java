99760d5b54ca03b05b47ee91ed4f72e518153797
ASUS <1761597780@qq.com>
2019-02-20 13:55:54 +0800

    ceshi晚饭

---
+++ app/src/main/java/com/example/asus/androidproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.asus.androidproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void read(View view){
+        startActivity(new Intent(this,read.class));
+    }
+}
---
+++ app/src/main/java/com/example/asus/androidproject/read.java
@@ -0,0 +1,36 @@
+package com.example.asus.androidproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.joanzapata.pdfview.PDFView;
+
+
+
+public class read extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+        PDFView pdfView=findViewById(R.id.cc);
+        pdfView.fromAsset("sheji.pdf")
+                .enableSwipe(true) // allows to block changing pages using swipe
+
+                .enableDoubletap(true)
+                .defaultPage(0)
+                // allows to draw something on the current page, usually visible in the middle of the screen
+                // allows to draw something on all pages, separately for every page. Called only for visible pages
+
+
+                // add dynamic spacing to fit each page on its own on the screen
+
+
+                 // toggle night mode
+                .load();
+        Log.v("cc","kaishi");
+
+
+    }
+}
